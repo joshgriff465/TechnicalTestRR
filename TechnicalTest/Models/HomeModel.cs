f98bfc47b1606907a1b413ae58f741e5982dbd00
joshua griffiths <joshgriffiths57@hotmail.com>
2022-07-18 12:10:59 +0100

    Updated using statements

--- TechnicalTest/Models/HomeModel.cs
+++ TechnicalTest/Models/HomeModel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TechnicalTest.SQL.Models;
 
 namespace TechnicalTest.Models
--- TechnicalTest/Startup.cs
+++ TechnicalTest/Startup.cs
@@ -2,6 +2,19 @@
 using Microsoft.AspNetCore.ResponseCompression;
 using System.IO.Compression;
 using TechnicalTest.SQL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace TechnicalTest
 {
